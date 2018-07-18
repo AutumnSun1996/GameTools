cdf972d72fc95a698bdb6b46c202533f6401c377
AutumnSun1996 <q19960105@163.com>
2018-07-18 17:13:59 +0800

    实时更新数据

--- 碧蓝航线/calcu.py
+++ 碧蓝航线/calcu.py
@@ -1,4 +1,5 @@
 import pyquery
+import requests
 import re
 import numpy as np
 import time
@@ -46,26 +47,60 @@ def accept(ship):
 
 nums = re.compile("(\d+\.)?\d+")
 
-with open("./输出.html", "r", -1, "UTF-8") as fl:
-    html = pyquery.PyQuery(fl.read())
+def get_info_online(entry):
+    TypeDict = {"zhanlie": "战列", "quzhu": "驱逐", "hangmu": "航母", "qingxun": "轻巡", "zhongxun": "重巡"}
+    # print("{!r} {!r} {!r}".format(entry, entry.parent(), entry.attr("class")))
+    ship_type = re.compile("\d+([a-z]+)shuchu").search(entry.parent().attr("class")).group(1)
+    info = [item.text() for item in entry("div.panel-body > p").items()]
+    cur_ship = {
+        "Name": info[0],
+        "Type": TypeDict.get(ship_type, "其他"),
+        "Extra": "",
+    }
+    if entry("div.panel-footer"):
+        cur_ship["Extra"] = "(" + entry("div.panel-footer").text() + ")"
+    for each in info[1:]:
+        idx = each.find("：")
+        cur_ship[each[:idx]] = float(re.compile("[.\d]+").search(each).group(0))
+    return cur_ship
 
-ship_types = "驱逐 轻巡 重巡 战列 航母 其他".split(" ")
-
-ship_list = []
-for span in html("td > span").items():
+def get_info_local(entry):
+    ship_types = "驱逐 轻巡 重巡 战列 航母 其他".split(" ")
     info = [item.text() for item in span("div.panel-body > p").items()]
     cur_ship = {
         "Name": info[0],
-        "Type": ship_types[len(span.parent().prevAll())-1],
+        "Type": ship_types[len(entry.parent().prevAll())-1],
         "Extra": "",
     }
-    if span("div.panel-footer"):
-        cur_ship["Extra"] = "(" + span("div.panel-footer").text() + ")"
+    if entry("div.panel-footer"):
+        cur_ship["Extra"] = "(" + entry("div.panel-footer").text() + ")"
     for each in info[1:]:
         idx = each.find("：")
         cur_ship[each[:idx]] = float(nums.search(each).group(0))
-    ship_list.append(cur_ship)
-    # print(cur_ship)
+    return cur_ship
+
+def get_list(url):
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Cookie": "Hm_lvt_dcb5060fba0123ff56d253331f28db6a=1531287514,1531290019,1531299417; Hm_lpvt_dcb5060fba0123ff56d253331f28db6a=1531299417",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+    }
+    try:
+        res = requests.get(url, headers=headers)
+        res.encoding = res.apparent_encoding
+    except Exception as e:
+        print("Get Local Content", e)
+        with open("./天梯榜数据.html", "r", -1, "UTF-8") as fl:
+            pq = pyquery.PyQuery(fl.read())
+    else:
+        print("Get Online Content")
+        with open("./天梯榜数据.html", "w", -1, "UTF-8") as fl:
+            fl.write(res.text)
+        pq = pyquery.PyQuery(res.text)
+    return [get_info_online(s) for s in pq("#LTputintable span.itemhover").items()]
+
+ship_list = get_list("http://wiki.joyme.com/blhx/%E7%A2%A7%E8%93%9D%E8%88%AA%E7%BA%BFWIKI%E5%A4%A9%E6%A2%AF%E6%A6%9C")
+print(len(ship_list))
 
 def harm_mean(arr, weight=None):
     arr = 1 / (np.array(arr) + 1)
@@ -91,7 +126,7 @@ def set_name_format(ship, size=10):
     if suffix > 0:
         ship["Name"] += (" " * suffix)
 
-for ship_type in ship_types:
+for ship_type in "驱逐 轻巡 重巡 战列 航母 其他".split(" "):
 # for ship_type in "驱逐 轻巡 重巡".split(" "):
     filted_ships = [ship for ship in ship_list if (ship["Type"] == ship_type and accept(ship))]
     filted_ships.sort(key=score)
