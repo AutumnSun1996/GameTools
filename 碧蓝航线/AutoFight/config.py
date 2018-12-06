e6f0c44663ae8c9597062e797b0101d6c08a7481
AutumnSun <q19960105@163.com>
2018-12-06 16:20:50 +0800

    reformat

--- 碧蓝航线/AutoFight/config.py
+++ 碧蓝航线/AutoFight/config.py
@@ -5,5 +5,6 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
                     format='%(asctime)s %(filename)s[%(lineno)d] - %(levelname)s: %(message)s')
 logger = logging.getLogger()
 
+
 class options:
     ORIGIN_WINDOW_SIZE = (1284, 752)
