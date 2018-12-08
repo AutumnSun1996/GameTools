7adf3274c42201ff7fe8f09ac1a6b3eb76e7e1c4
AutumnSun1996 <q19960105@163.com>
2018-12-08 12:57:32 +0800

    继承关系修改 增加虚拟战斗次数

--- 碧蓝航线/AutoFight/fight_sp4.py
+++ 碧蓝航线/AutoFight/fight_sp4.py
@@ -5,7 +5,6 @@ import time
 
 from config import logger
 from map_anchor import FightMap
-from blhx import AzurLaneControl
 
 ANCHORS = [
     {
@@ -52,80 +51,88 @@ SCENE_MAP = {
 }
 
 
-class SP4Control(AzurLaneControl):
+class SP4Control(FightMap):
     def __init__(self):
-        AzurLaneControl.__init__(self)
+        super().__init__(ANCHORS)
         self.scene_list.append(SCENE_MAP)
-        self.map = FightMap(self.hwnd, ANCHORS)
-
+        self.fight_idx_offset = 0
+    
+    def get_fight_index(self):
+        return super().get_fight_index() + self.fight_idx_offset
+    
     def reset_fight_index(self):
         fight_idx = self.get_fight_index()
         mod = fight_idx % 6
         logger.warning("Current Fight Index: %d (%d)", fight_idx, mod)
         if mod != 0:
-            self.set_fight_index(fight_idx+mod)
-            logger.warning("Reset Fight Index From %d To %d", fight_idx, fight_idx+mod)
+            self.set_fight_index(fight_idx+6-mod)
+            logger.warning("Reset Fight Index From %d To %d", fight_idx, fight_idx+6-mod)
 
     def fight(self):
+        if self.last_scene['Name'] == "战斗地图":
+            logger.info("停留在战斗界面. 增加虚拟战斗次数")
+            logger.info("history: %s", [scene["Name"] for scene in self.scene_history])
+            self.fight_idx_offset += 1
         fight_idx = self.get_fight_index()
-        logger.info("Fight index: %d", fight_idx)
-        fight_idx = fight_idx % 6
-        if fight_idx == 0:
+        mod = fight_idx % 6
+        logger.info("Fight index: %d (%d)", fight_idx, mod)
+        if mod == 0:
             logger.info("触发扫描-E6")
-            self.map.click_at_map("E6")
+            self.click_at_map("E6")
             # click_at(645, 460, hwnd)
-            time.sleep(10)
+            time.sleep(8)
             logger.info("前往待命-E4")
-            self.map.click_at_map("E4")
+            self.click_at_map("E4")
             # click_at(645, 280, hwnd)
-            time.sleep(4)
+            time.sleep(3)
             logger.info("切换2队")
             self.toggle_fleet()
             # reset_map_pos()
             # time.sleep(2)
             logger.info("左边精英-D4")
-            self.map.click_at_map("D4")
+            self.click_at_map("D4")
             # click_at(535, 300, hwnd)
             # self.set_fight_index(1)
             time.sleep(6)
-        elif fight_idx == 1:
+        elif mod == 1:
             # reset_map_pos()
             logger.info("左边小舰队-C5")
-            self.map.click_at_map("C5")
+            time.sleep(2)
+            self.click_at_map("C5")
             # click_at(400, 390, hwnd)
             # self.set_fight_index(2)
             time.sleep(2)
-        elif fight_idx == 2:
+        elif mod == 2:
             # reset_map_pos()
             logger.info("上边小舰队-E3")
-            self.map.click_at_map("E3")
+            self.click_at_map("E3")
             # click_at(665, 210, hwnd)
             # self.set_fight_index(3)
             time.sleep(4)
-        elif fight_idx == 3:
+        elif mod == 3:
             # reset_map_pos()
             logger.info("右边精英-F4")
-            self.map.click_at_map("F4")
+            self.click_at_map("F4")
             # click_at(790, 300, hwnd)
             # self.set_fight_index(4)
             time.sleep(2)
-        elif fight_idx == 4:
+        elif mod == 4:
             # reset_map_pos()
             logger.info("右边小舰队-G5")
-            self.map.click_at_map("G5")
+            self.click_at_map("G5")
             # click_at(925, 400, hwnd)
             # self.set_fight_index(5)
             time.sleep(2)
-        elif fight_idx == 5:
+        elif mod == 5:
             logger.info("切换1队")
             self.toggle_fleet()
             # reset_map_pos()
             logger.info("Boss-E1")
-            self.map.click_at_map("E1")
+            self.click_at_map("E1")
             # click_at(655, 80, hwnd)
             # self.set_fight_index(0)
             time.sleep(2)
-        time.sleep(2)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
