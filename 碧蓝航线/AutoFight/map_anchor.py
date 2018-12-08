f0cd16493523a030a6fbd798c1ce06253ea602b7
AutumnSun1996 <q19960105@163.com>
2018-12-08 12:59:08 +0800

    类继承关系修改 寻敌前增加场景检查

--- 碧蓝航线/AutoFight/map_anchor.py
+++ 碧蓝航线/AutoFight/map_anchor.py
@@ -7,8 +7,16 @@ from config import logger
 from image_tools import cv_imread, get_match, get_window_shot
 from win32_tools import drag, click_at
 
+from blhx import AzurLaneControl
+
 cell_width = 120
 cell_height = 90
+
+
+FIGHT_MAP = {
+    "Name": "战斗地图",
+    "Compare": [{"Rect": (840, 670, 1280, 740), "Name": "切换-迎击.png", "TreshHold": 5}]
+}
 
 def ord_distance(a, b):
     # 计算2个棋盘坐标的加权曼哈顿距离
@@ -30,9 +38,9 @@ def get_map_pos(origin, origin_name, target_name):
     return x, y
 
 
-class FightMap:
-    def __init__(self, hwnd, anchors, ):
-        self.hwnd = hwnd
+class FightMap(AzurLaneControl):
+    def __init__(self, anchors):
+        super().__init__()
         self.anchors = anchors
 
     def move_map_to(self, x, y):
@@ -42,8 +50,7 @@ class FightMap:
         drag(self.hwnd, (580, 380), (580 + diff_x, 380 + diff_y))
         time.sleep(1)
 
-    def locate_anchor(self, target):
-        image = get_window_shot(self.hwnd)
+    def locate_anchor(self, target, image):
         self.anchors.sort(key=lambda a: ord_distance(a['OnMap'], target))
         for anchor in self.anchors:
             anchor_img = cv_imread(os.path.join("images", anchor['Name']))
@@ -56,13 +63,17 @@ class FightMap:
             logger.debug("未找到%s. 最优结果%.3f.", anchor['Name'], diff)
 
     def click_at_map(self, target, repeat=0):
+        image = get_window_shot(self.hwnd)
+        if not self.scene_match_check(FIGHT_MAP, image):
+            logger.warning("非战斗地图. 停止搜索目标")
+            return
         if repeat >= 5:
             raise Exception("地图搜索失败")
 
         # 根据棋盘坐标点击战斗地图
         logger.debug("搜索%s", target)
 
-        target_pos = self.locate_anchor(target)
+        target_pos = self.locate_anchor(target, image)
         if not target_pos:
             logger.debug("未找到anchor, 重置地图")
             self.move_map_to(2000, 1000)
