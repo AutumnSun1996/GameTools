e1ee152377fc8b2c8796611a47bc7aec90fd41b7
AutumnSun <q19960105@163.com>
2018-12-11 10:54:32 +0800

    根据计算机名获取配置

--- 碧蓝航线/AutoFight/config.py
+++ 碧蓝航线/AutoFight/config.py
@@ -5,9 +5,10 @@ import os
 import sys
 import json
 import logging
+import socket
 from configparser import ConfigParser
 
-config_file = "config-1280x720.ini"
+config_file = "config-Company.ini"
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s %(filename)s[%(lineno)d] - %(levelname)s: %(message)s')
@@ -18,6 +19,14 @@ class Config(ConfigParser):
     def save(self):
         with open(config_file, "w", -1, "UTF-8") as fl:
             self.write(fl)
-        
+
+    def init_device(self, hostname):
+        pass
+
+
 config = Config()
 config.read(config_file, encoding="UTF-8")
+
+hostname = "Device_"+socket.gethostname()
+if not config.has_section(hostname):
+    config.init_device(hostname)
