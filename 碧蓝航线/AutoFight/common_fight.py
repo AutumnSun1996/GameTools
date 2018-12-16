0aae62125f50436322db2dae806633e393a9f5aa
AutumnSun <q19960105@163.com>
2018-12-16 18:29:36 +0800

    每次进图重置地图信息

--- 碧蓝航线/AutoFight/common_fight.py
+++ 碧蓝航线/AutoFight/common_fight.py
@@ -15,12 +15,41 @@ class CommonMap(FightMap):
 
     def __init__(self, map_name=None):
         super().__init__(map_name)
+        self.virtual_fight_index = 5
+        self.reset_map_data()
+
+    def parse_fight_condition(self, condition):
+        if isinstance(condition, list):
+            if condition[0] == 'FightIndex':
+                fidx = self.get_fight_index() + self.virtual_fight_index
+                mod = fidx % 6
+                logger.debug("FightIndex %d(%d)", fidx, mod)
+                return mod
+            if condition[0] == 'not':
+                return not self.parse_fight_condition(condition[1])
+            if condition[0] == '==':
+                return self.parse_fight_condition(condition[1]) == self.parse_fight_condition(condition[2])
+            if condition[0] == 'in':
+                return self.parse_fight_condition(condition[1]) in self.parse_fight_condition(condition[2])
+            if condition[0] == '>=':
+                return self.parse_fight_condition(condition[1]) >= self.parse_fight_condition(condition[2])
+            if condition[0] == '<=':
+                return self.parse_fight_condition(condition[1]) <= self.parse_fight_condition(condition[2])
+            if condition[0] == '!=':
+                return self.parse_fight_condition(condition[1]) != self.parse_fight_condition(condition[2])
+        return condition
+
+    def reset_map_data(self):
+        """进入战场，重置虚拟战斗次数，使下一次战斗正常开始
+        """
+
         self.g = nx.Graph()
         self.boss = []
+        self.other_fleet = None
+        self.current_fleet = None
         self.resource_points = []
         self.enemies = set()
         self.pos = {}
-        self.virtual_fight_index = 6
         for j, row in enumerate(self.data['Map']):
             for i, cell_type in enumerate(row):
                 name = chr(ord('A')+i) + str(j+1)
@@ -43,39 +72,16 @@ class CommonMap(FightMap):
         for node in self.g:
             if self.g.nodes[node]['cell_type'] == 'E':
                 self.set_enemy(node, 'Possible')
-    
-    def parse_fight_condition(self, condition):
-        if isinstance(condition, list):
-            if condition[0] == 'FightIndex':
-                fidx = self.get_fight_index() + self.virtual_fight_index
-                mod = fidx % 6
-                logger.debug("FightIndex %d(%d)", fidx, mod)
-                return mod
-            if condition[0] == 'not':
-                return not self.parse_fight_condition(condition[1])
-            if condition[0] == '==':
-                return self.parse_fight_condition(condition[1]) == self.parse_fight_condition(condition[2])
-            if condition[0] == 'in':
-                return self.parse_fight_condition(condition[1]) in self.parse_fight_condition(condition[2])
-            if condition[0] == '>=':
-                return self.parse_fight_condition(condition[1]) >= self.parse_fight_condition(condition[2])
-            if condition[0] == '<=':
-                return self.parse_fight_condition(condition[1]) <= self.parse_fight_condition(condition[2])
-            if condition[0] == '!=':
-                return self.parse_fight_condition(condition[1]) != self.parse_fight_condition(condition[2])
-        return condition
 
     def reset_fight_index(self):
-        """进入战场，重置虚拟战斗次数，使下一次战斗正常开始
-        """
         # self.virtual_fight_index = 0
+        self.reset_map_data()
         fight_idx = self.get_fight_index() + self.virtual_fight_index
         mod = fight_idx % 6
         logger.warning("Current Fight Index: %d (%d)", fight_idx, mod)
         if mod != 0:
             self.virtual_fight_index += 6-mod
             logger.info("Reset Fight Index From %d To %d", fight_idx, fight_idx+6-mod)
-
 
     def enemies_on_path(self, path):
         enemies = []
@@ -141,11 +147,11 @@ class CommonMap(FightMap):
             edges.append((path[i-1], path[i]))
         nx.draw_networkx_edges(self.g.to_directed(), pos=self.pos, edgelist=edges, edge_color='r')
 
-    def shortest_path(self, start, target):
-        return nx.shortest_path(self.g, start, target, 'weight')
+    def shortest_path(self, start, target, weight='weight'):
+        return nx.shortest_path(self.g, start, target, weight)
 
-    def shortest_path_length(self, start, target):
-        return nx.shortest_path_length(self.g, start, target, 'weight')
+    def shortest_path_length(self, start, target, weight='weight'):
+        return nx.shortest_path_length(self.g, start, target, weight)
 
     def common_path(self, start, targets):
         path = []
@@ -180,27 +186,45 @@ class CommonMap(FightMap):
         return choice
 
     def next_enemy(self, source):
+        if not self.enemies:
+            self.recheck_full_map()
+            return self.next_enemy(source)
         for f in source:
             for b in self.boss + self.resource_points:
                 path = self.shortest_path(f, b)
                 enemies = self.enemies_on_path(path)
                 if enemies:
-                    logger.info("选择挡路的敌人(%s)", enemies)
+                    logger.info("选择挡住%s->%s所有路径的敌人(%s)", f, b, enemies)
                     return enemies[0]
-        cur = source[0]
+        for f in source:
+            for b in self.boss + self.resource_points:
+                path = self.shortest_path(f, b, None)
+                enemies = self.enemies_on_path(path)
+                if enemies:
+                    logger.info("选择挡住%s->%s最短路径的敌人(%s)", f, b, enemies)
+                    return enemies[0]
+
         distance = []
         for enemy in self.enemies:
-            distance.append([self.shortest_path_length(cur, enemy), enemy])
+            distance.append([self.shortest_path_length(self.current_fleet, enemy), enemy])
         distance.sort()
         logger.info("选择最近的敌人: %s", distance[0][1])
         return distance[0][1]
 
     def search_for_boss(self, repeat=0):
+        logger.info("搜索Boss%d", repeat)
         if repeat >= 5:
             self.critical("Boss搜索失败")
 
-        ret, pos = self.resource_in_screen('Boss')
-        if ret:
+        if len(self.boss) > 1:
+            ret, pos = self.resource_in_screen('Boss')
+            if not ret:
+                ret, pos = self.resource_in_screen('Boss-Bigger')
+            if not ret:
+                self.recheck_full_map()
+                self.search_for_boss(repeat+1)
+                return
+            logger.info("找到Boss %s", pos)
             x, y = pos
             x_min, y_min, x_max, y_max = self.get_resource_rect("可移动区域")
             if x < x_min or x > x_max or y < y_min or y > y_max:
@@ -208,23 +232,66 @@ class CommonMap(FightMap):
                 self.move_map_to(x, y)
                 self.search_for_boss(repeat+1)
                 return
-            dx, dy = self.resources['Boss']['Offset']
             w, h = self.resources['Boss']['Size']
-            logger.info("找到Boss")
-            rand_click(self.hwnd, (x+dx, y+dy, x+dx+w, y+dy+h))
+            rand_click(self.hwnd, (x, y, x+w, y+h))
             self.wait(6)
             return
-        self.wait_mannual("未找到Boss")
+        self.click_at_map(self.boss[0])
+
+    def check_map(self):
+        self.make_screen_shot()
+        anchor_name, anchor_pos = self.get_best_anchor()
+        if not self.parse_condition(self.scenes['战斗地图']['Condition']):
+            self.notice("Not in 战斗地图")
+            return
+
+        enemies = self.find_on_map(anchor_name, anchor_pos, 'Lv', False)
+        for enemy in enemies:
+            self.set_enemy(enemy)
+        logger.info("找到敌人: %s", enemies)
+
+        boss = self.find_on_map(anchor_name, anchor_pos, 'Boss', False)
+        if len(boss) != 1:
+            boss = self.find_on_map(anchor_name, anchor_pos, 'Boss-Bigger', False)
+        if len(boss) == 1:
+            self.boss = list(boss)
+            logger.info("找到Boss: %s", boss)
+
+        pointer = self.find_on_map(anchor_name, anchor_pos, 'Pointer', False)
+        if pointer:
+            self.current_fleet = list(pointer)[0]
+            logger.info("找到当前舰队: %s", self.current_fleet)
+
+        fleets = self.find_on_map(anchor_name, anchor_pos, 'Ammo', False)
+        logger.info("找到舰队: %s", fleets)
+        if self.current_fleet in fleets:
+            fleets.discard(self.current_fleet)
+        if len(fleets) == 1:
+            self.other_fleet = list(fleets)[0]
+            logger.info("找到另一舰队: %s", self.other_fleet)
+
+        if self.current_fleet and self.current_fleet in self.enemies:
+            self.set_enemy(self.current_fleet, 'Defeated')
+        if self.other_fleet and self.other_fleet in self.enemies:
+            self.set_enemy(self.other_fleet, 'Defeated')
 
     def recheck_full_map(self):
-        # TODO: 地图信息更新
-        pass
+        logger.warning("地图信息更新")
+        for target in self.data['ViewPort']:
+            logger.info("移动到%s", target)
+            self.move_map_to(*self.locate_target(target)[1])
+            self.check_map()
 
     def after_bonus(self):
         # TODO: 前往问号点后判断获得道具, 获得维修
-        pass
+        scene = self.update_current_scene(['获得道具', '战斗地图'])
+        if scene['Name'] == '获得道具':
+            self.do_actions(scene['Actions'])
+            if self.resource_in_screen('收起右侧菜单')[0]:
+                self.click_at_resource('收起右侧菜单')
+            self.update_current_scene(['战斗地图'])
 
-    def search_for_bonus(self,  repeat=0):
+    def search_for_bonus(self, repeat=0):
         if repeat >= 5:
             self.critical("问号点搜索失败")
         ret, pos = self.resource_in_screen('问号点')
@@ -233,7 +300,7 @@ class CommonMap(FightMap):
             self.recheck_full_map()
             self.search_for_bonus(repeat+1)
             return
-        
+
         x, y = pos
         x_min, y_min, x_max, y_max = self.get_resource_rect("可移动区域")
         if x < x_min or x > x_max or y < y_min or y > y_max:
@@ -241,7 +308,7 @@ class CommonMap(FightMap):
             self.move_map_to(x, y)
             self.search_for_bonus(repeat+1)
             return
-        
+
         dx, dy = self.resources['问号点']['Offset']
         w, h = self.resources['问号点']['Size']
         logger.info("找到问号点")
@@ -257,51 +324,34 @@ class CommonMap(FightMap):
         for item in self.data['Strategy']:
             if self.parse_fight_condition(item['Condition']):
                 logger.info("战斗策略：%s", item)
-                for action in item['Actions']:
-                    if action == "NormalFight":
-                        self.normal_fight()
-                    elif action == "问号点":
-                        self.search_for_bonus()
-                    elif action == "Boss":
-                        self.search_for_boss()
-                    elif action == "ToggleFleet":
-                        self.click_at_resource("切换舰队")
-                    else:
-                        self.error("Unkown Action %s", action)
+                self.do_actions(item['Actions'])
 
-    def normal_fight(self):
-        anchor_name, anchor_pos = self.get_best_anchor()
-        # boss = self.find_on_map(anchor_name, anchor_pos, 'Boss')
-        # if boss:
-        #     self.try_for_boss(boss)
-        #     return
-        self.make_screen_shot()
-        if not self.parse_condition(self.scenes['战斗地图']['Condition']):
-            self.notice("Not in 战斗地图")
+    def click_at_map(self, target):
+        FightMap.click_at_map(self, target)
+        path = self.shortest_path(self.current_fleet, target)
+        wait = (len(path)-1) * 0.7
+        self.wait(wait)
+        if target == self.other_fleet:
+            self.other_fleet = self.current_fleet
+        self.current_fleet = target
+        if target in self.enemies:
+            self.set_enemy(target, 'Defeated')
+
+    def normal_fight(self, repeat=0):
+        if repeat >= 5:
+            self.error("地图处理失败")
             return
-        for enemy in self.find_on_map(anchor_name, anchor_pos, 'Lv', False):
-            self.set_enemy(enemy)
-        fleets = self.find_on_map(anchor_name, anchor_pos, 'Ammo', False)
-        for fleet in fleets:
-            if fleet in list(self.enemies):
-                self.set_enemy(fleet, 'Defeated')
-        logger.info("找到敌人 %s", self.enemies)
 
-        current_fleet = self.find_on_map(anchor_name, anchor_pos, 'Pointer', False)
-        if current_fleet:
-            current_fleet = list(current_fleet)[0]
-        else:
-            current_fleet = list(fleets)[0]
+        if not self.current_fleet:
+            self.recheck_full_map()
+            self.normal_fight(repeat+1)
+            return
 
-        fleets.discard(current_fleet)
-        if fleets:
-            other_fleet = list(fleets)[0]
-            logger.info("当前舰队: %s, 另一舰队: %s", current_fleet, other_fleet)
-            self.click_at_map(self.next_enemy([current_fleet, other_fleet]))
-        else:
-            logger.info("当前舰队: %s", current_fleet)
-            self.click_at_map(self.next_enemy([current_fleet]))
-        self.wait(2)
+        fleets = [self.current_fleet]
+        if self.other_fleet:
+            fleets.append(self.other_fleet)
+
+        self.click_at_map(self.next_enemy(fleets))
 
 
 if __name__ == "__main__":
