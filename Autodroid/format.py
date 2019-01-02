32ce050f4d055218a45f5835f96d69bd0a49ac9d
AutumnSun <q19960105@163.com>
2019-01-02 17:07:32 +0800

    调整格式化参数

---
+++ Autodroid/format.py
@@ -0,0 +1,79 @@
+import os
+import shutil
+import json
+
+config = {"MaxLineWidth": 80}
+
+
+def text_width(text):
+    width = sum([1 if ord(char) < 0x7f else 2 for char in text])
+    return width
+
+
+def dumps(obj):
+    return json.dumps(obj, indent=None, ensure_ascii=False)
+
+
+def encode(obj, prefix, level):
+    js_text = prefix + dumps(obj)
+    if isinstance(obj, (str, int, float)):
+        result = js_text
+    elif level * 2 + text_width(js_text) < config["MaxLineWidth"]:
+        # print("Direct:", js_text)
+        result = js_text
+    # 宽度大于120
+    elif isinstance(obj, dict):
+        lines = []
+        for key, value in obj.items():
+            key_str = ('  ' * level) + '  ' + dumps(key) + ": "
+            lines.append(encode(value, key_str, level+1))
+            # print("For", key, "Get",)
+            # print(lines[-1])
+        result = prefix + '{\n' + ',\n'.join(lines) + '\n' + ('  ' * level) + '}'
+    elif isinstance(obj, (list, tuple)):
+        lines = []
+        for value in obj:
+            key_str = ('  ' * level) + '  '
+            lines.append(encode(value, key_str, level+1))
+            # print("For", key, "Get",)
+            # print(lines[-1])
+        result = prefix + '[\n' + ',\n'.join(lines) + '\n' + ('  ' * level) + ']'
+    else:
+        raise TypeError("Can't Handle %s" % obj)
+    # print('obj:', obj, 'prefix:', prefix, 'level:', level, 'Result:', result, sep='\n')
+    return result
+
+
+def jsonformat(path, backup=True):
+    # path = os.path.realpath(path)
+    if backup:
+        basename, ext = os.path.splitext(path)
+        shutil.copy2(path, basename + '.bak' + ext)
+    with open(path, "r", -1, "UTF-8") as fl:
+        content = fl.read()
+    new_content = encode(json.loads(content), '', 0)
+    if content == new_content:
+        print("No Change: ", path)
+        return
+    with open(path, "w", -1, "UTF-8") as fl:
+        fl.write(new_content)
+    print("Reformated:", path)
+
+
+def do_format(folder, max_width=80):
+    config["MaxLineWidth"] = max_width
+    for root, folders, files in os.walk(folder):
+        if "resources" in root:
+            continue
+        for name in files:
+            if name.endswith(".bak.json"):
+                continue
+            if not name.endswith(".json"):
+                continue
+            target = os.path.join(root, name)
+            jsonformat(target)
+
+
+if __name__ == "__main__":
+    do_format("fgo", 80)
+    do_format("azurlane", 70)
