e589e457b277d3ae8f6927ae88ef88ca23b3948e
AutumnSun <q19960105@163.com>
2019-01-03 19:10:38 +0800

    添加check_map_anchor

--- Autodroid/notebook/azurlane.py
+++ Autodroid/notebook/azurlane.py
@@ -4,8 +4,24 @@ from azurlane.common_fight import FightMap
 
 const["section"] = "azurlane"
 
-def check_anchor_on_map(data, on_map):
-    pass
+
+def check_map_anchor(anchor):
+    s = const["s"]
+    if isinstance(anchor, str):
+        anchor = s.data["Anchors"][anchor]
+    diff, pos = get_match(s.screen, anchor['ImageData'])
+    print(diff, pos)
+    x, y = pos
+    w, h = anchor["Size"]
+    dx, dy = anchor["Offset"]
+    draw = s.screen.copy()
+    for node_name in s.g.nodes:
+        nx, ny = s.get_map_pos(anchor["OnMap"], (x+dx, y+dy), node_name)
+        cv.circle(draw, (nx, ny), 3, (0, 0, 0), -1)
+    cv.rectangle(draw, (x, y), (x+w, y+h), (255, 255, 255), 2)
+    cv.circle(draw, (x+dx, y+dy), 5, (255, 255, 255), -1)
+    show(draw)
+
 
 def init_map(name):
     const["s"] = FightMap(name)
