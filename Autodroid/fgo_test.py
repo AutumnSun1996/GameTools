c07b314724b6640f4b05b03f646a92f9461c4c61
AutumnSun1996 <q19960105@163.com>
2019-02-03 18:46:26 +0800

    使用手动控制

--- Autodroid/fgo_test.py
+++ Autodroid/fgo_test.py
@@ -1,7 +1,26 @@
+import time
+
+import win32api
+
 from fgo.fgo_simple import FGOSimple
+
+class ManualFight(FGOSimple):
+    def __init__(self):
+        FGOSimple.__init__(self, "手动")
+        self.last_manual = 0
+
+    @property
+    def since_last_manual(self):
+        return time.time() - self.last_manual
+
+    def manual(self):
+        if self.scene_changed or self.since_last_manual > 30:
+            self.go_top()
+            win32api.MessageBeep()
+            self.last_manual = time.time()
 
 
 if __name__ == "__main__":
-    f = FGOSimple("每日任务")
+    f = ManualFight()
     while True:
         f.check_scene()
