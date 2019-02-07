c0b25682b4a712f4b55542cf766f70257fe94b40
AutumnSun1996 <q19960105@163.com>
2019-02-07 20:30:31 +0800

    修改退役选择方式

--- Autodroid/azurlane/azurlane.py
+++ Autodroid/azurlane/azurlane.py
@@ -5,18 +5,63 @@ By AutumnSun
 """
 import time
 import json
+from collections import defaultdict
 
 from config_loader import logger, config
 from simulator import SimulatorControl
 from simulator.win32_tools import rand_click
-from collections import defaultdict
+from simulator.image_tools import cv_save
+from ocr.baidu_ocr import ocr, contact_images
 
 class AzurLaneControl(SimulatorControl):
     """碧蓝航线通用控制
     """
     section = "AzurLane"
     status_path = "%s/data/fightStatus.json" % section
-    retire_choices = ["退役-白色舰娘", "退役-蓝色舰娘"]
+    retire_choices = {
+        # 驱逐-普通
+        "卡辛", "唐斯", "克雷文", "麦考尔", "奥利克", "富特", "斯彭斯", "小猎兔犬", "大斗犬", 
+        "彗星", "新月", "小天鹅", "狐提", "蒲", "松", "樟", "楙", "杌", "檧", "Z20",
+        # 驱逐-稀有
+        "格里德利", "弗莱彻", "撒切尔", "本森", "西姆斯", "哈曼", "拉德福特", "杰金斯", "布什", 
+        "黑泽伍德", "霍比", "科尔克", "女将", "热心", "命运女神", "天后", 
+        "枫", "梓", "柏", "梿", "棭", "蓉", "藮", "榊", "棡", "枨", "萩", 
+        "槆", "柉", "樇", "栭", "栘", "樋", "Z18","Z19", "福尔班", "勒马尔", 
+        # 驱逐-精锐
+        "萤火虫", "标枪", "Z23", "拉菲", "柚", "查尔斯·奥斯本", 
+        
+        # 轻巡-普通
+        "奥马哈", "罗利", "里士满", "利安得", "貊", "貃", "柯尼斯堡", "卡尔斯鲁厄", "科隆", 
+        # 轻巡-稀有
+        "布鲁克林", "菲尼克斯", "亚特兰大", "朱诺", "孟菲斯", "康克德",  "阿基里斯", "阿贾克斯", 
+        "阿瑞托莎", "加拉蒂亚", "斐济", "纽卡斯尔", "--牙买加", "貉", "--豻", 
+        # 轻巡-精锐
+        "海伦娜", "克利夫兰", "欧若拉", "逸仙", "宁海", "貎", 
+        # 轻巡-超稀有
+        "圣地亚哥", "贝尔法斯特", 
+        
+        # 重巡-普通
+        "彭萨科拉", "盐湖城", "狼", "狌", "犹", "猅", 
+        # 重巡-稀有
+        "北安普敦", "芝加哥", "波特兰", "什罗普郡", "肯特", "萨福克", "诺福克", "獌", "狏", "苏塞克斯", 
+        # 重巡-精锐
+        "休斯敦", "印第安纳波利斯", "伦敦", "埃克塞特", "约克",
+        
+        # 战
+        "内华达", "俄克拉荷马", 
+        "反击", "宾夕法尼亚", "田纳西", "加利福尼亚", "鲼", 
+        "声望", "伊丽莎白女王", "罗德尼",
+        "胡德", "华盛顿",
+        
+        # 航
+        "博格", "兰利", "突击者", "竞技神", 
+        "长岛", "鹞", 
+        "独角兽", "枭", "皇家方舟", "龙",
+        "企业", "光辉", "埃塞克斯",
+        
+        # 其他
+        "女灶神", "黑暗界", "茗",
+    }
 
     def get_fight_status(self):
         """战斗次数计数"""
@@ -28,14 +73,14 @@ class AzurLaneControl(SimulatorControl):
             status = {"VirtualFightIndex": 0, "TrueFightIndex": 0, "FightIndex": 0}
         return status
 
-    def inc_fight_index(self):
+    def inc_fight_index(self, inc=1):
         """增加战斗次数"""
         status = self.get_fight_status()
-        logger.debug("增加Fight Index: %d -> %d", status["TrueFightIndex"], status["TrueFightIndex"]+1)
-        status["TrueFightIndex"] += 1
-        self.seve_fight_status(status)
+        logger.debug("增加Fight Index: %d -> %d", status["TrueFightIndex"], status["TrueFightIndex"]+inc)
+        status["TrueFightIndex"] += inc
+        self.save_fight_status(status)
 
-    def seve_fight_status(self, status):
+    def save_fight_status(self, status):
         """设置战斗次数"""
         with open(self.status_path, 'w') as fl:
             json.dump(status, fl, ensure_ascii=False)
@@ -70,14 +115,34 @@ class AzurLaneControl(SimulatorControl):
     def select_ships(self):
         """选择退役舰娘"""
         self.make_screen_shot()
-        targets = []
-        for name in self.retire_choices:
-            dx, dy = self.resources[name].get("ClickOffset", (-10, -10))
-            w, h = self.resources[name].get("ClickSize", (20, 20))
-            for x, y in self.search_resource(name)[1]:
-                targets.append((x+dx, y+dy, x+dx+w, y+dy+h))
-        logger.info("select_ships: %s", targets)
-        return targets[:10]
+        images = []
+        clicks = []
+        retire = self.resources["退役选择"]
+        dx, dy = retire.get("ClickOffset", (0,0))
+        dw, dh = retire.get("ClickSize", retire["Size"])
+
+        for x, y in retire["Positions"]:
+            images.append(self.crop_resource("退役选择", offset=(x, y)))
+            clicks.append((x+dx, y+dy, x+dx+dw, y+dy+dh))
+        
+        name_image = contact_images(*images)
+        names = ocr.image2text(name_image)
+        if len(names) != len(images):
+            cv_save("logs/OCR-Error.png", name_image)
+            names = ocr.image2text_accurate(name_image)
+            self.notice("OCR Results %s Do Not Match Input Image. Retry with accurate ocr." % names, "OCR-Error")
+            if len(names) != len(images):
+                self.error("Accurate OCR Results %s Do Not Match Input Image." % names, "OCR-Error")
+                return []
+
+        results = []
+        for idx in range(len(names)):
+            if names[idx] in self.retire_choices:
+                logger.info("select_ship: Choose %s", names[idx])
+                results.append(clicks[idx])
+            else:
+                logger.info("select_ship: Skip %s", names[idx])
+        return results[:10]
 
     def retire(self):
         """执行退役操作"""
