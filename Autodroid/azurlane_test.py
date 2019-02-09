10b1eb6b9c413f984a704ca85aba87b51516e62b
AutumnSun1996 <q19960105@163.com>
2019-02-09 16:11:44 +0800

    使用自动脚本

--- Autodroid/azurlane_test.py
+++ Autodroid/azurlane_test.py
@@ -1,35 +1,7 @@
-import time
-
-import win32api
-
-from azurlane.common_fight import CommonMap
-
-try:
-    __builtin__ = __builtins__
-except NameError:
-    pass
-
-
-class ManualFight(CommonMap):
-    def __init__(self):
-        CommonMap.__init__(self, "通用地图")
-        self.last_manual = 0
-
-    @property
-    def since_last_manual(self):
-        return time.time() - self.last_manual
-
-    def manual(self):
-        if self.scene_changed or self.since_last_manual > 30:
-            self.go_top()
-            win32api.MessageBeep()
-            self.last_manual = time.time()
+from azurlane.fight_event import EventFight as AzurLane
 
 
 if __name__ == "__main__":
-    print(__builtin__)
-    import datetime
-    m = ManualFight()
-    start_index = m.get_fight_status()["FightIndex"]
+    m = AzurLane("凛冬D3")
     while True:
         m.check_scene()
