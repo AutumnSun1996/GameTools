9204810f4494500868f7cd92727ad209b22464b5
AutumnSun1996 <q19960105@163.com>
2019-02-13 19:55:58 +0800

    boss搜索算法调整

--- Autodroid/azurlane/fight_event.py
+++ Autodroid/azurlane/fight_event.py
@@ -5,6 +5,9 @@ from .common_fight import CommonMap, FightMap, logger
 
 class EventFight(CommonMap):
     def toggle_on_map(self, target, side):
+        if not self.current_fleet:
+            self.recheck_full_map()
+            
         if self.current_fleet == target:
             self.goto(side)
         else:
@@ -17,7 +20,7 @@ class EventFight(CommonMap):
             path = path[:self.data["FleetMoveStep"]+1]
             target = path[-1]
 
-        FightMap.click_at_map(self, target)
+        CommonMap.click_at_map(self, target)
         wait = (len(path)-1) * 0.7
         self.wait(wait)
         if target == self.other_fleet:
@@ -50,23 +53,31 @@ class EventFight(CommonMap):
             self.search_for_boss(repeat+1, idx)
             return
             
-        if idx is not None:
-            target = self.data['ViewPort'][idx]
-            logger.info("检查区域%s", target)
-            self.move_map_to(*self.locate_target(target)[1])
-        if repeat >= len(self.data['ViewPort']):
-            self.critical("Boss搜索失败")
+        if not isinstance(self.boss, str):
+            if idx is not None:
+                target = self.data['ViewPort'][idx]
+                logger.info("检查区域%s", target)
+                self.move_map_to(*self.locate_target(target)[1])
+            if repeat >= len(self.data['ViewPort']):
+                self.critical("Boss搜索失败")
 
+            anchor_name, anchor_pos = self.get_best_anchor()
             
-        anchor_name, anchor_pos = self.get_best_anchor()
-        
-        pos = self.find_on_map(anchor_name, anchor_pos, 'Boss')
-        if not pos:
-            pos = self.find_on_map(anchor_name, anchor_pos, 'Boss-Bigger')
-        if not pos:
-            self.search_for_boss(repeat, idx+1)
-            return
-        logger.info("找到Boss %s", pos)
-        target = list(pos)[0]
-        self.goto(target)
+            pos = self.find_on_map(anchor_name, anchor_pos, 'Boss')
+            if not pos and "Boss2" in self.resources:
+                pos = self.find_on_map(anchor_name, anchor_pos, 'Boss2')
+            if not pos and "Boss3" in self.resources:
+                pos = self.find_on_map(anchor_name, anchor_pos, 'Boss3')
+            if not pos:
+                self.search_for_boss(repeat, idx+1)
+                return
+            boss = list(pos)[0]
+            if boss in self.boss:
+                self.boss = boss
+            else:
+                logger.warning("False Boss %s not in %s", boss, self.boss)
+                self.search_for_boss(repeat, idx+1)
+                return
+        logger.info("找到Boss %s", self.boss)
+        self.goto(self.boss)
         return
