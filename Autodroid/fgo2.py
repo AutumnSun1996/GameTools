9bd444134eec6d8bd07e37df8de5393f8fa74354
AutumnSun1996 <q19960105@163.com>
2019-02-13 19:57:26 +0800

    石头号算法更新

--- Autodroid/fgo2.py
+++ Autodroid/fgo2.py
@@ -1,13 +1,10 @@
-# 石头号-弓阶
-
 from fgo_fast import FGOSimple
 
-FGOSimple.section = "FGO2"
-
-
 if __name__ == "__main__":
-    fgo = FGOSimple("C尼禄")
-    # fgo = FGOSimple("石头号-弓阶")
+    FGOSimple.section = "FGO2"
+    # fgo = FGOSimple("C尼禄")
+    # fgo = FGOSimple("R金时")
+    fgo = FGOSimple("石头号-弓阶")
     count = 0
     while 1:
         fgo.check_scene()
@@ -15,6 +12,6 @@ if __name__ == "__main__":
         if fgo.current_scene_name == "获得物品":
             count += 1
             print(fgo.current_scene_name, count)
-            if count >= 1:
+            if count >= 40:
                 fgo.manual()
                 break
