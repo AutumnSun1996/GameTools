5f6960c1098d5d70a45f823f27eec5d1f92fe83e
AutumnSun <q19960105@163.com>
2019-02-18 17:49:20 +0800

    地图更新

--- Autodroid/fgo3.py
+++ Autodroid/fgo3.py
@@ -2,7 +2,7 @@ from fgo_fast import FGOSimple
 
 if __name__ == "__main__":
     FGOSimple.section = "FGO3"
-    fgo = FGOSimple("FGO3")
+    fgo = FGOSimple("XY赝作速刷-枪本")
     count = 0
     while 1:
         fgo.check_scene()
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         if fgo.current_scene_name == "获得物品":
             count += 1
             print(fgo.current_scene_name, count)
-            if count >= 50:
+            if count >= 20:
                 fgo.manual()
                 break
--- Autodroid/fgo_fast.py
+++ Autodroid/fgo_fast.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     # fgo = FGOSimple("刷材料")
     # fgo = FGOSimple("主号换人礼装速刷")
     # fgo = FGOSimple("主号赝作速刷")
-    fgo = FGOSimple("主号赝作速刷2")
+    fgo = FGOSimple("主号赝作速刷-枪本")
 
     count = 0
     while 1:
@@ -13,6 +13,6 @@ if __name__ == "__main__":
         if fgo.current_scene_name == "获得物品":
             count += 1
             print(fgo.current_scene_name, count)
-            if count >= 5:
-                fgo.manual()
+            if count >= 10:
+                # fgo.manual()
                 break
