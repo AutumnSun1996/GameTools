9cf8a3c44e18cb2c859ef1d2861b56856e9039ca
AutumnSun <q19960105@163.com>
2019-02-18 21:18:30 +0800

    修改地图

--- Autodroid/fgo_fast.py
+++ Autodroid/fgo_fast.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     # fgo = FGOSimple("刷材料")
     # fgo = FGOSimple("主号换人礼装速刷")
     # fgo = FGOSimple("主号赝作速刷")
-    fgo = FGOSimple("主号赝作速刷-枪本")
+    # fgo = FGOSimple("主号赝作速刷-枪本")
+    fgo = FGOSimple("主号赝作速刷-骑本")
 
     count = 0
     while 1:
@@ -13,6 +14,6 @@ if __name__ == "__main__":
         if fgo.current_scene_name == "获得物品":
             count += 1
             print(fgo.current_scene_name, count)
-            if count >= 10:
+            if count >= 20:
                 # fgo.manual()
                 break
