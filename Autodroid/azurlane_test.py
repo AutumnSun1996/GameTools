bab3363da08a7c5ac2a4cd55780d2340452f6ed7
AutumnSun1996 <q19960105@163.com>
2019-02-19 08:11:41 +0800

    修改目标地图

--- Autodroid/azurlane_test.py
+++ Autodroid/azurlane_test.py
@@ -1,7 +1,7 @@
-from azurlane.fight_event import EventFight as AzurLane
+from azurlane.fight_mainline import MainFight as AzurLane
 
 
 if __name__ == "__main__":
-    m = AzurLane("凛冬D3")
+    m = AzurLane("11-2")
     while True:
         m.check_scene()
