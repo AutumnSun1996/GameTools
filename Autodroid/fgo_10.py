cee8d520bebfbc270a99a611568f9b491b4e6677
AutumnSun <q19960105@163.com>
2019-02-21 15:58:37 +0800

    修改交互

--- Autodroid/fgo_10.py
+++ Autodroid/fgo_10.py
@@ -12,11 +12,14 @@ if __name__ == "__main__":
     count = 0
     while True:
         fgo.check_scene()
-        if fgo.current_scene_name in {"10连抽", "关卡选择"}:
+        if fgo.current_scene_name in {"重置奖品"}:
             count += 1
             print(fgo.current_scene_name, count)
         elif fgo.current_scene_name == "结束":
-            fgo.notice("已完成%d次抽取。等待手动操作" % count)
+            fgo.notice("抽取结束(已重置%d次), 等待手动操作" % count)
             print("Total:", count)
             input("Pause...")
+        elif fgo.current_scene_name == "礼物盒已满":
+            fgo.notice("礼物盒已满(已重置%d次), 等待手动操作" % count)
+            break
     print("Total:", count)
