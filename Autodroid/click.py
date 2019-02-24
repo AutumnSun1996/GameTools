eae4c5c237825319f138e223ac735e8e41484b41
AutumnSun <q19960105@163.com>
2019-02-24 18:47:41 +0800

    20次点击

---
+++ Autodroid/click.py
@@ -0,0 +1,34 @@
+import sys
+import time
+
+from simulator.win32_tools import rand_click, get_window_hwnd
+from simulator.image_tools import get_all_match
+from config_loader import config
+
+
+def main(section):
+    window_title = config.get(section, "WindowTitle")
+    hwnd = get_window_hwnd(window_title)
+
+    if not hwnd:
+        print("No Such Window")
+        return
+
+    x0, y0 = 100, 200
+    dx, dy = int(800/6), int(430/3)
+    w, h = 80, 80
+    for i in range(7):
+        for j in range(3):
+            x, y = (x0+dx*i, y0+dy*j)
+            rand_click(hwnd, (x, y, x+w, y+h))
+            time.sleep(0.3)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        section = sys.argv[1]
+    else:
+        section = "FGO"
+    while True:
+        main(section)
+        input("Pause...")
