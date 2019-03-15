75ee98322b32297a12854bc8a001019d7480e7f7
AutumnSun <q19960105@163.com>
2019-03-15 14:41:51 +0800

    change map

--- Autodroid/azurlane_test.py
+++ Autodroid/azurlane_test.py
@@ -1,7 +1,7 @@
-from azurlane.fight_mainline import MainFight as AzurLane
+from azurlane.fight_simple import SimpleFight as AzurLane
 
 
 if __name__ == "__main__":
-    m = AzurLane("11-2")
+    m = AzurLane("峡湾间的星辰SP3")
     while True:
         m.check_scene()
