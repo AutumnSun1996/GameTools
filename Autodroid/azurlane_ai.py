497d3a9035329b895846c8b6d9aaababe36f3a01
AutumnSun <q19960105@163.com>
2019-05-01 18:36:26 +0800

    绊爱联动SP4配置

--- Autodroid/azurlane_ai.py
+++ Autodroid/azurlane_ai.py
@@ -5,7 +5,7 @@ import win32api
 from azurlane.fight_simple import CommonMap
 
 if __name__ == "__main__":
-    m = CommonMap("无伏击地图")
+    m = CommonMap("AI-SP4")
     start_index = m.get_fight_status()["FightIndex"]
     while True:
         m.check_scene()
