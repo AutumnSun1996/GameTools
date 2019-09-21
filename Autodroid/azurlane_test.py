662894af47b59328746d99a610d79ebb93e83837
AutumnSun <q19960105@163.com>
2019-09-21 10:53:20 +0800

    删除无用启动脚本

--- Autodroid/azurlane_test.py
+++
@@ -1,8 +0,0 @@
-# from azurlane.fight_simple import SimpleFight as AzurLane
-from azurlane.operation import Operation as AzurLane
-
-
-if __name__ == "__main__":
-    m = AzurLane("演习")
-    while True:
-        m.check_scene()
