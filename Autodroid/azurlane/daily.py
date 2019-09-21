0fa8d9edafea53f01ea765f29dba6cb2665f5ee0
AutumnSun <q19960105@163.com>
2019-09-21 10:42:08 +0800

    取消手动导入默认配置步骤

--- Autodroid/azurlane/daily.py
+++ Autodroid/azurlane/daily.py
@@ -14,15 +14,6 @@ filter_kernel = np.array([[-4, -2, -4], [-2, 24, -2], [-4, -2, -4]])
 
 
 class Daily(AzurLaneControl):
-    def __init__(self, map_name):
-        super(Daily, self).__init__()
-        self.map_name = map_name
-        self.data = load_map(self.map_name, self.section)
-        logger.info("Update Resources %s", self.data['Resources'].keys())
-        self.resources.update(self.data['Resources'])
-        logger.info("Update Scenes %s", self.data['Scenes'].keys())
-        self.scenes.update(self.data['Scenes'])
-
     def check_one(self, part):
         part = cv.resize(part, (200, 300))
         filtered = cv.filter2D(part, 0, filter_kernel)
