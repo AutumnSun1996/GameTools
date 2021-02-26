f654f71201e0e24d64cc9752380f8ee915f8f3e6
qiushiyang <qiushiyang@corp.netease.com>
2021-02-26 19:48:14 +0800

    feat: 实现js版本mml解析

---
+++ ty/js/mmlparser.js
@@ -0,0 +1,162 @@
+"use strict";
+
+class Scanner {
+    constructor(source) {
+        this.source = source;
+        this.index = 0;
+    }
+
+    hasNext() {
+        return this.index < this.source.length;
+    }
+
+    peek() {
+        return this.source.charAt(this.index) || "";
+    }
+
+    next() {
+        return this.source.charAt(this.index++) || "";
+    }
+
+    forward() {
+        while (this.hasNext() && this.match(/\s/)) {
+            ++this.index;
+        }
+    }
+
+    expect(matcher) {
+        if (!this.match(matcher)) {
+            this.throwUnexpectedToken();
+        }
+        this.index += 1;
+    }
+
+    scan(matcher) {
+        const target = this.source.substr(this.index);
+
+        let result = matcher.exec(target);
+
+        if (result) {
+            this.index += result[0].length;
+        }
+
+        return result;
+    }
+
+    throwUnexpectedToken() {
+        const identifier = this.peek() || "ILLEGAL";
+        throw new SyntaxError(`Unexpected token: ${identifier}`);
+    }
+
+    part(offset = 0, size = 10) {
+        return this.source.substr(this.index + offset, size);
+    }
+}
+
+const NOTE_MAP = { "C": 0, "D": 2, "E": 4, "F": 5, "G": 7, "A": 9, "B": 11 };
+
+function parse(source) {
+    let scanner = new Scanner(source);
+    let result = [];
+    let prevNoteNum = null, prevDuration = 0;
+    let state = {
+        tempo: 120,
+        divide: 4,
+        vel: 90,
+        octave: 4,
+        timestamp: 0,
+    };
+    while (scanner.hasNext()) {
+        let m = null;
+        // Note
+        m = scanner.scan(/^([A-GPR])([\-\+#]?)(\d*)(\.*)(\*\d+)?(\[[12]\])?(:?)(&?)/i);
+        if (m) {
+            let note = m[1].toUpperCase(), noteNum = null, evt = {};
+            if (note === "P" || note === "R") {
+                evt = { type: "rest", timestamp: state.timestamp };
+                noteNum = "r";
+            } else {
+                noteNum = NOTE_MAP[note] + 12 * state.octave;
+                if (m[2]) { // acc
+                    if (m[2] === "-") {
+                        --noteNum;
+                    } else {
+                        ++noteNum;
+                    }
+                }
+                evt = { type: "note", note: `${m[1]}${state.octave}`, noteNum: noteNum, timestamp: state.timestamp, vel: state.vel };
+            }
+            let divide = state.divide;
+            if (m[3]) {
+                divide = parseInt(m[3]);
+            }
+            evt.duration = 240 / state.tempo / divide;
+            if (prevNoteNum !== null) {
+                if (prevNoteNum !== noteNum) {
+                    throw SyntaxError(`相连的音符必须相同:  ${prevNoteNum} ${noteNum} @ ${scanner.part(-5, 0)}`)
+                }
+                evt.duration = evt.duration + prevDuration;
+            }
+            if (m[8]) {
+                prevNoteNum = noteNum;
+                prevDuration = evt.duration;
+                console.log("pre note", evt);
+            } else {
+                prevNoteNum = null;
+                prevDuration = null;
+                if (!m[7]) {
+                    state.timestamp += evt.duration;
+                }
+                result.push(evt);
+            }
+            continue;
+        }
+        if (scanner.scan(/^>/)) {
+            ++state.octave;
+            result.push({ type: 'octave', value: state.octave });
+            continue;
+        }
+        if (scanner.scan(/^</)) {
+            --state.octave;
+            result.push({ type: 'octave', value: state.octave });
+            continue;
+        }
+        m = scanner.scan(/^O(\d+)/i)
+        if (m) {
+            state.octave = parseInt(m[1]);
+            result.push({ type: 'octave', value: state.octave });
+            continue;
+        }
+        m = scanner.scan(/^L(\d+)/i)
+        if (m) {
+            state.divide = parseInt(m[1]);
+            result.push({ type: 'divide', value: state.divide });
+            continue;
+        }
+        m = scanner.scan(/^V(\d+)/i);
+        if (m) {
+            state.vel = parseInt(m[1]);
+            result.push({ type: 'vel', value: state.vel });
+            continue;
+        }
+        m = scanner.scan(/^T(\d+)/i);
+        if (m) {
+            console.log("set tempo", m);
+            state.tempo = parseInt(m[1]);
+            result.push({ type: 'tempo', value: state.vel });
+            continue;
+        }
+        m = scanner.scan(/^#.+$/m);
+        if (m) {
+            result.push({ type: 'comment', value: m[0] });
+            continue;
+        }
+        m = scanner.scan(/^[|\s]+/);
+        if (m) {
+            result.push({ type: 'ignore', value: m[0] });
+            continue;
+        }
+        throw SyntaxError(`无法解析: ${scanner.part()} ${result}`);
+    }
+    return result;
+}
