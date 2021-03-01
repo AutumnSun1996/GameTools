7701f977bc32d85de49103c703fe411a6dd079f7
qiushiyang <qiushiyang@corp.netease.com>
2021-03-01 10:40:52 +0800

    feat: 新增用于辅助重建的pos信息

--- ty/js/mmlparser.js
+++ ty/js/mmlparser.js
@@ -77,6 +77,11 @@ function numToNote(num) {
     return `${note}${octave}`;
 }
 
+/**
+ * 将类简谱文本转化为指令列表
+ * 
+ * @param {String} text 将类简谱文本
+ */
 function numTextToCommands(text) {
     const DIVIDE_MAP = { "%": 4, "%%": 2, "%%%%": 1, "_": 8, "__": 16, "___": 32, "A": 1, "B": 2, "C": 4, "D": 8, "E": 16, "F": 32 };
     let scanner = new Scanner(text);
@@ -84,14 +89,14 @@ function numTextToCommands(text) {
     let state = initState();
     while (scanner.hasNext()) {
         let m = null;
+        let pos = scanner.index;
         // Note
         m = scanner.scan(/^([0-7])([-+]?)(%+|_+|[ABCDEF])?(\.*)(\*\d+)?(\[[12]\])?(:?)(&?)/);
         console.log(m);
         if (m) {
             let note = m[1].toUpperCase(), noteNum = null, evt = {};
             if (note === "0") {
-                evt = { type: "rest" };
-                noteNum = "r";
+                evt = { type: "rest", pos };
             } else {
                 noteNum = NOTE_MAP[note] + 12 * state.octave + 12 + state.offset;
                 if (m[2]) { // acc
@@ -101,7 +106,7 @@ function numTextToCommands(text) {
                         ++noteNum;
                     }
                 }
-                evt = { type: "note", noteNum: noteNum };
+                evt = { type: "note", noteNum, pos };
                 evt.note = numToNote(noteNum);
             }
 
@@ -163,30 +168,30 @@ function numTextToCommands(text) {
         m = scanner.scan(/^V(\d+)/i);
         if (m) {
             state.vel = parseInt(m[1]);
-            commands.push({ type: 'vel', value: state.vel });
+            commands.push({ type: 'vel', value: state.vel, pos });
             continue;
         }
         m = scanner.scan(/^T(\d+)/i);
         if (m) {
             console.log("set tempo", m);
             state.tempo = parseInt(m[1]);
-            commands.push({ type: 'tempo', value: state.tempo });
+            commands.push({ type: 'tempo', value: state.tempo, pos });
             continue;
         }
         m = scanner.scan(/^#\s*NewTrack\n/);
         if (m) {
             state = initState();
-            commands.push({ type: 'newtrack', text: m[0] });
+            commands.push({ type: 'newtrack', text: m[0], pos });
             continue;
         }
         m = scanner.scan(/^#.*(\n|$)/);
         if (m) {
-            commands.push({ type: 'text', text: m[0] });
+            commands.push({ type: 'text', text: m[0], pos });
             continue;
         }
         m = scanner.scan(/^[|\s]/);
         if (m) {
-            commands.push({ type: 'text', text: m[0] });
+            commands.push({ type: 'text', text: m[0], pos });
             continue;
         }
         console.log(commands);
@@ -201,16 +206,16 @@ function textToCommands(text) {
     let state = initState();
     while (scanner.hasNext()) {
         let m = null;
+        let pos = scanner.index;
         // Note
         m = scanner.scan(/^(N\d+|[A-GPR])([\-\+#]?)(\d*)(\.*)(\*\d+)?(\[[12]\])?(:?)(&?)/i);
         if (m) {
             let note = m[1].toUpperCase(), noteNum = null, evt = {};
             if (note === "P" || note === "R") {
-                evt = { type: "rest" };
-                noteNum = "r";
+                evt = { type: "rest", pos };
             } else if (note[0] === "N") {
                 noteNum = parseInt(note.substr(1));
-                evt = { type: "note", noteNum: noteNum };
+                evt = { type: "note", noteNum, pos };
                 evt.note = numToNote(noteNum);
             } else {
                 noteNum = NOTE_MAP[note] + 12 * state.octave + 12;
@@ -221,7 +226,7 @@ function textToCommands(text) {
                         ++noteNum;
                     }
                 }
-                evt = { type: "note", noteNum: noteNum };
+                evt = { type: "note", noteNum, pos };
                 evt.note = numToNote(noteNum);
             }
 
@@ -266,30 +271,30 @@ function textToCommands(text) {
         m = scanner.scan(/^V(\d+)/i);
         if (m) {
             state.vel = parseInt(m[1]);
-            commands.push({ type: 'vel', value: state.vel });
+            commands.push({ type: 'vel', value: state.vel, pos });
             continue;
         }
         m = scanner.scan(/^T(\d+)/i);
         if (m) {
             console.log("set tempo", m);
             state.tempo = parseInt(m[1]);
-            commands.push({ type: 'tempo', value: state.tempo });
+            commands.push({ type: 'tempo', value: state.tempo, pos });
             continue;
         }
         m = scanner.scan(/^#\s*NewTrack\n/);
         if (m) {
             state = initState();
-            commands.push({ type: 'newtrack', text: m[0] });
+            commands.push({ type: 'newtrack', text: m[0], pos });
             continue;
         }
         m = scanner.scan(/^#.+(\n|$)/);
         if (m) {
-            commands.push({ type: 'text', text: m[0] });
+            commands.push({ type: 'text', text: m[0], pos });
             continue;
         }
         m = scanner.scan(/^[|\s]/);
         if (m) {
-            commands.push({ type: 'text', text: m[0] });
+            commands.push({ type: 'text', text: m[0], pos });
             continue;
         }
         console.log(commands);
