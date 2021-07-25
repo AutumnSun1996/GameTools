2444cb859baab4701da20bbe001cc1bddf5ce841
AutumnSun1996 <q19960105@163.com>
2021-07-25 21:51:34 +0800

    feat: 使用max_fight_count在maps内部进行结束判断

--- Autodroid/al_main.py
+++ Autodroid/al_main.py
@@ -12,33 +12,12 @@ from azurlane import load_map
 logger = logging.getLogger(__name__)
 
 
-def make_stop_checker(args):
-    def stop_checker(s):
-
-        if s.current_scene_name in ("外部地图", "再次前往") and not s.actions_done:
-            now = datetime.now()
-            if (
-                args.end_in is not None
-                and not s.actions_done
-                and now - args.start_time > args.end_in
-            ):
-                return True
-            fcs = s.data.get('FightCountScene', '获得道具')
-            if (
-                args.fight_count is not None
-                and s.scene_history_count[fcs] >= args.fight_count
-            ):
-                return True
-        return False
-
-    return stop_checker
-
-
 def main(args):
     az = load_map(args.map_name)
     az.no_quiet = args.no_quiet
+    az.max_fight_count = args.fight_count
     try:
-        az.main_loop(make_stop_checker(args))
+        az.main_loop()
         # input("Pause...")
     except KeyboardInterrupt as e:
         logger.warning("(OnExit) Scene Count: %s", az.scene_history_count)
